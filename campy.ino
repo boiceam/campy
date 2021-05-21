197c067f08d3d59e3e23049fb1388287faefeb12
Chris Hasson <hassoncs@gmail.com>
2021-05-20 21:22:08 -0700

    Get show transitions working properly

--- campy.ino
+++ campy.ino
@@ -19,6 +19,7 @@
 #define LOOP_DELAY_MS    33
 
 #define SHOW_ROTATION_DURATION     5000 // ms
+#define SHOW_TRANSITION_DURATION     500 // ms
 
 // Declare the NeoPixel strip object
 Adafruit_NeoPixel strip(LED_COUNT_ALL, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -37,15 +38,43 @@ uint32_t Show_Bounce_Up_And_Down(long time_ms, size_t light_index) {
   }
 }
 
-uint32_t Show_A(long current_time_ms, size_t light_index) {
-  return GetWheelColor(light_index);
+uint32_t Show_Bounce_Up_And_Down_Rainbow(long time_ms, size_t light_index) {
+  uint8_t on_light = abs((LED_COUNT - 1) - (time_ms / 65) % (LED_COUNT * 2 - 1));
+  if (light_index == on_light) {
+    return GetWheelColor((time_ms / 25 + light_index * 2) % (256));
+  } else {
+    return strip.Color(0, 0, 0);
+  }
+}
+
+uint32_t Show_Rainbow_Cycle(long time_ms, size_t light_index) {
+  return GetWheelColor((time_ms / 25 + light_index * 2) % (256));
+}
+
+uint32_t Show_Pulse(long time_ms, size_t light_index) {
+  uint8_t n = abs(256 - (time_ms % 2500) / 2500. * 512);
+  return strip.Color(n, n, n);
+}
+
+uint32_t Show_Red(long time_ms, size_t light_index) {
+  return strip.Color(255, 0, 0);
+}
+
+uint32_t Show_Blue(long time_ms, size_t light_index) {
+  return strip.Color(0, 0, 255);
+}
+
+uint32_t Show_White(long time_ms, size_t light_index) {
+  return strip.Color(255, 255, 255);
 }
 
 // ----------
 
-uint32_t (*SHOWS[]) (long current_time_ms, size_t light_index) = {
-  Show_A,
+uint32_t (*SHOWS[]) (long time_ms, size_t light_index) = {
+  Show_Rainbow_Cycle,
   Show_Bounce_Up_And_Down,
+  Show_Pulse,
+  Show_Bounce_Up_And_Down_Rainbow,
 };
 size_t SHOW_COUNT = sizeof(SHOWS) / sizeof(SHOWS[0]);
 
@@ -75,16 +104,23 @@ void loop() {
   unsigned long current_time_ms = millis();
 
   unsigned long show_index = (current_time_ms / SHOW_ROTATION_DURATION) % SHOW_COUNT;
+  float show_transition_fraction = min(1.,
+    max(0, ((int32_t)(current_time_ms % SHOW_ROTATION_DURATION) - (SHOW_ROTATION_DURATION - SHOW_TRANSITION_DURATION))) / (float)SHOW_TRANSITION_DURATION
+  );
+  Serial.println("show_index: " + String(show_index) + ", fraction..." + String(show_transition_fraction));
 
   // Update the color of the pixels in the buffer
   for (size_t i = 0; i < strip.numPixels(); i++) {
     // Show the same show on both sides, so pass in the mirrored version if i to the show
-    uint32_t light_index = i % LED_COUNT;
+    uint8_t light_index = i % LED_COUNT;
     if (i >= LED_COUNT) {
       light_index = LED_COUNT - light_index - 1;
     }
-    uint32_t color = (*SHOWS[show_index])(current_time_ms, light_index);
-    strip.setPixelColor(i, color);
+    uint32_t colorA = (*SHOWS[show_index])(current_time_ms, light_index);
+    uint32_t colorB = (*SHOWS[(show_index + 1) % SHOW_COUNT])(current_time_ms, light_index);
+    uint32_t colorBlended = interpolate(colorA, colorB, show_transition_fraction * 255);
+
+    strip.setPixelColor(i, colorBlended);
   }
 
   // send the color update to the LEDs
@@ -112,4 +148,20 @@ uint32_t GetWheelColor(uint8_t position) {
     position -= 170;
     return strip.Color(position * 3, 255 - position * 3, 0);
   }
+}
+
+uint32_t interpolate(uint32_t colA, uint32_t colorB, uint8_t percent) {
+  uint16_t rA = (colA >> 16) & 0x000000FF;
+  uint16_t gA = (colA >> 8) & 0x000000FF;
+  uint16_t bA = (colA >> 0) & 0x000000FF;
+  uint16_t rB = (colorB >> 16) & 0x000000FF;
+  uint16_t gB = (colorB >> 8) & 0x000000FF;
+  uint16_t bB = (colorB >> 0) & 0x000000FF;
+  uint8_t inverse = 255 - percent;
+
+ return strip.Color(
+   (rA * 255 * inverse + rB * 255 * percent) / (255*255),
+   (gA * 255 * inverse + gB * 255 * percent) / (255*255),
+   (bA * 255 * inverse + bB * 255 * percent) / (255*255)
+  );
 }
