4b3080c79da44ab56d681d7ddd3168278d9ea5cc
Andrew Boice <boiceam@gmail.com>
2021-06-01 16:13:10 -0700

    Programmed version

--- campy.ino
+++ campy.ino
@@ -6,6 +6,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <RTCZero.h>
 
+
 #define LED_PIN          0
 #define LED_COUNT_ALL    20
 #define LED_COUNT        10
@@ -15,7 +16,7 @@
 // When in low power mode programming mode must be entered using the boot pads on the device
 // Recommend only enabling low power mode once development is complete
 #define LOW_POWER         1
-#define TIME_DEBUG        1
+#define DEBUG             0
 
 #define PROGRAMMING_WINDOW_MS       (20*1000) // ms
 
@@ -25,9 +26,7 @@
 #define SHOW_ROTATION_DURATION      (15 * 60 * 1000) // ms
 #define SHOW_TRANSITION_DURATION    (5000) // ms
 
-//
 // Sleep schedule
-//
 #define SLEEP_DELAY_MS          (4 * 1000) // ms
 #define SLEEP_ON_DURATION       (10) // hours
 
@@ -45,6 +44,9 @@ unsigned long cum_sleep_time = 0;
 bool allow_deepsleep = false;
 bool usb_attached = false;
 unsigned long last_time_log = 0;
+unsigned long last_tick_time = 0;
+uint8_t active_show_number = 0;
+
 
 // ===========================================================================
 uint32_t Show_Rainbow_Bouncing_Dot(long time_ms, size_t light_index);
@@ -66,7 +68,7 @@ void setup() {
   // Initialize the RTC, if power has been lost (POR or BOR) this will set the RTC to 1/1/00 00:00
   // If power was not lost the previous time will be restored (though ~4 seconds may be lost)
   rtc.begin();
-
+  
   // Turn on the built-in status LED intially
   // After the programming window elaspes it will go off to save power
   pinMode(LED_BUILTIN, OUTPUT);
@@ -78,8 +80,12 @@ void setup() {
   SerialUSB.println("Starting Campy...");
   SerialUSB.flush();
 
- #ifdef CRYSTALLESS
-  SerialUSB.println("CRYSTALLESS");
+#if DEBUG
+  Serial1.begin(115200);
+  Serial1.println("Starting Campy...");
+  Serial1.print("PM->RCAUSE = ");
+  Serial1.println(PM->RCAUSE.reg, HEX);
+  LogCurrentTime();
 #endif
 
   // Setup the LED strip and clear the color buffer initially
@@ -106,17 +112,12 @@ void loop() {
   // This number will overflow after approximately 50 days.
   unsigned long current_time_ms = (millis() + cum_sleep_time);
 
-#if TIME_DEBUG
-  if (last_time_log + (60*1000) < current_time_ms) {
+#if DEBUG
+  if (last_time_log + (60 * 1000) < current_time_ms) {
     last_time_log = current_time_ms;
-    LoggingStart();
-    char buf[20];
-    sprintf(buf, "%02d:%02d:%02d", rtc.getHours(), rtc.getMinutes(), rtc.getSeconds());
-    SerialUSB.println(buf);
-    LoggingStop();
+    LogCurrentTime();
   }
 #endif
-
 
   // Run the light show for SLEEP_ON_DURATION, then sleep until SLEEP_DAY_DURATION
   bool show_should_run = rtc.getHours() < SLEEP_ON_DURATION;
@@ -127,12 +128,27 @@ void loop() {
     return;
   }
 
-  unsigned long show_index = (current_time_ms / SHOW_ROTATION_DURATION) % SHOW_COUNT;
-  float show_transition_fraction = min(1.,
-                                       max(0, ((int32_t)(current_time_ms % SHOW_ROTATION_DURATION) - (SHOW_ROTATION_DURATION - SHOW_TRANSITION_DURATION))) / (float)SHOW_TRANSITION_DURATION
-                                      );
-  // Serial.println("show_index: " + String(show_index) + ", fraction..." + String(show_transition_fraction));
+  uint8_t next_show_number;
+  if (rtc.getMinutes() < 15) {
+    next_show_number = 0;
+  } else if (rtc.getMinutes() < 30) {
+    next_show_number = 1;
+  } else if (rtc.getMinutes() < 45) {
+    next_show_number = 2;
+  } else {
+    next_show_number = 3;
+  }
 
+  if (next_show_number != active_show_number) {
+  #if DEBUG
+    Serial1.print("Show change: ");
+    Serial1.print(active_show_number);
+    Serial1.print(" -> ");
+    Serial1.println(next_show_number);
+  #endif
+    active_show_number = next_show_number;
+  }
+  
   // Update the color of the pixels in the buffer
   for (size_t i = 0; i < strip.numPixels(); i++) {
     // Show the same show on both sides, so pass in the mirrored version if i to the show
@@ -140,10 +156,8 @@ void loop() {
     if (i >= LED_COUNT) {
       light_index = LED_COUNT - light_index - 1;
     }
-    uint32_t colorA = (*SHOWS[show_index])(current_time_ms, light_index);
-    uint32_t colorB = (*SHOWS[(show_index + 1) % SHOW_COUNT])(current_time_ms, light_index);
-    uint32_t colorBlended = interpolate(colorA, colorB, show_transition_fraction * 255);
-    uint32_t colorCorrected = strip.gamma32(colorBlended);
+    uint32_t color = (*SHOWS[active_show_number])(current_time_ms, light_index);
+    uint32_t colorCorrected = strip.gamma32(color);
     strip.setPixelColor(i, colorCorrected);
   }
 
@@ -153,6 +167,12 @@ void loop() {
   cum_sleep_time += sleepFor(LOOP_DELAY_MS, allow_deepsleep);
 }
 
+void LogCurrentTime(){
+    char buf[20];
+    sprintf(buf, "%02d:%02d:%02d", rtc.getHours(), rtc.getMinutes(), rtc.getSeconds());
+    Serial1.println(buf);
+}
+
 void LoggingStart() {
   if (!usb_attached) {
     USBDevice.init();
@@ -160,17 +180,17 @@ void LoggingStart() {
     // Delay to allow the connection to establish
     delay(2000);
     usb_attached = true;
-    
+
     // Connect serial USB connection to allow for debugging
     SerialUSB.begin(115200);
   }
 }
 
 void LoggingStop() {
-  if(!allow_deepsleep){
+  if (!allow_deepsleep) {
     return;
   }
-  
+
   if (usb_attached) {
     SerialUSB.flush();
     delay(100);
